a97e87cce538fa3e84f63d5199426eccc0446033
Brendan Schreiner <brendanpschreiner@gmail.com>
2019-01-29 22:13:29 -0600

    added responseHandler utility

---
+++ src/utils/responseHandler.js
@@ -0,0 +1,45 @@
+const logger = require('./logger')
+
+const header = {
+  'Content-Type': 'application/json'
+}
+
+function successResponse(body, statusCode = 200) {
+  logger.info({
+    body,
+    statusCode
+  }, 'Successful Response')
+  return {
+    Headers: header,
+    Status: statusCode,
+    Body: JSON.stringify(body)
+  }
+}
+
+function failureResponse(error, message, statusCode) {
+  logger.error({
+    error,
+    message,
+    statusCode
+  }, 'Failed Response')
+  let status
+  if (statusCode >= 400 && statusCode < 500 ) {
+    status = statusCode
+  } else {
+    status = 500
+  }
+  return {
+    Headers: header,
+    Status: status,
+    Body: JSON.stringify({
+      message
+    })
+  }
+}
+
+failureResponse('garbo', 'hi you fucked up', 404)
+
+module.exports = {
+  successResponse,
+  failureResponse
+}
